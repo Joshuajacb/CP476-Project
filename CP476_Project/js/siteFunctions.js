bab11aff477c1159af1c3bfe11f066f4f5b2be92
Joshuajacb <31943652+Joshuajacb@users.noreply.github.com>
2019-03-03 22:01:41 -0500

    Initial Upload

---
+++ CP476_Project/js/siteFunctions.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+    $('.components li').on('click', function() {
+    	$('.components li').removeClass('active');
+    	$(this).addClass('active');
+    });
+
+});
